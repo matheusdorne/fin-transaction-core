3590238e5c1ecca82dee1b4bb941164a6c065495
Matheus Dornelles <matheus.dorne@gmail.com>
2026-01-10 19:15:49 -0300

    feat: assert queue for notification in test

--- tests/Feature/TransferServiceTest.php
+++ tests/Feature/TransferServiceTest.php
@@ -3,11 +3,13 @@
 namespace Tests\Feature;
 
 use App\Enums\TransactionStatus;
+use App\Jobs\SendNotificationJob;
 use App\Models\User;
 use App\Models\Wallet;
 use App\Services\TransferService;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 class TransferServiceTest extends TestCase
@@ -21,15 +23,16 @@ class TransferServiceTest extends TestCase
                 'data' => ['authorization' => true],
             ], 200),
         ]);
+        Queue::fake();
 
         $sender = User::factory()->create();
-        $payerWallet = Wallet::factory()->create([
+        $senderWallet = Wallet::factory()->create([
             'user_id' => $sender->id,
             'balance' => 100.00,
         ]);
 
         $receiver = User::factory()->create();
-        $payeeWallet = Wallet::factory()->create([
+        $receiverWallet = Wallet::factory()->create([
             'user_id' => $receiver->id,
             'balance' => 0.00,
         ]);
@@ -37,15 +40,20 @@ class TransferServiceTest extends TestCase
         $service = app(TransferService::class);
         $transaction = $service->transfer($sender->id, $receiver->id, 50.00);
 
-        $this->assertEquals(50.00, $payerWallet->fresh()->balance);
-        $this->assertEquals(50.00, $payeeWallet->fresh()->balance);
+        $this->assertEquals(50.00, $senderWallet->fresh()->balance);
+        $this->assertEquals(50.00, $receiverWallet->fresh()->balance);
 
         $this->assertDatabaseHas('transactions', [
             'id' => $transaction->id,
-            'sender_wallet_id' => $payerWallet->id,
-            'receiver_wallet_id' => $payeeWallet->id,
+            'sender_wallet_id' => $senderWallet->id,
+            'receiver_wallet_id' => $receiverWallet->id,
             'amount' => 50.00,
             'status' => TransactionStatus::COMPLETED->value,
         ]);
+
+        Queue::assertPushed(SendNotificationJob::class, function ($job) use ($senderWallet, $receiverWallet) {
+            return $job->transaction->sender_wallet_id === $senderWallet->id
+            || $job->transaction->receiver_wallet_id === $receiverWallet->id;
+        });
     }
 }
