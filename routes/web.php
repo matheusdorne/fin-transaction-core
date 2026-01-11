0f8f193cd3caecc3caa48f74e0eab8f7eec52a7c
Matheus Dornelles <matheus.dorne@gmail.com>
2026-01-11 12:09:11 -0300

    feat: add dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransferController;
 use App\Models\User;
 use App\Models\Wallet;
 use Illuminate\Foundation\Application;
@@ -34,6 +35,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/transfer', [TransferController::class, 'store'])->name('transfer.store');
 });
 
 require __DIR__.'/auth.php';
