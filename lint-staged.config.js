07eae508666f45cecf327587ed07bdd502db1589
Matheus Dornelles <matheus.dorne@gmail.com>
2026-01-11 12:08:36 -0300

    fix: lint docker path issues

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+export default {
+    "**/*.php": (filenames) => {
+        const relativeFiles = filenames.map((file) =>
+            file.replace(process.cwd() + "/", ""),
+        );
+
+        return `./vendor/bin/sail bin pint ${relativeFiles.join("")}`;
+    },
+    "**/*.{js,vue,html,css,json}": ["prettier --write"],
+};
