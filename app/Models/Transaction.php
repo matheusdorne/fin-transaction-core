8246e3c450db65f1f42655032ccd92bee8e28c7e
Matheus Dornelles <matheus.dorne@gmail.com>
2026-01-18 20:34:27 -0300

    feat(transactions): add relations

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\TransactionStatus;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -21,4 +22,14 @@ class Transaction extends Model
         'status' => TransactionStatus::class,
         'amount' => 'decimal:4',
     ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(Wallet::class, 'sender_wallet_id');
+    }
+
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(Wallet::class, 'receiver_wallet_id');
+    }
 }
